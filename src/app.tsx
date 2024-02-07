cb33ad83eb2dcf48e92f90e0687a0ddc6f0de60d
Flávio Porfírio <flavioporfirio86@gmail.com>
2024-02-07 20:06:46 -0300

    feat: home structure and components

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
@@ -13,6 +15,18 @@ export function App() {
       </form>
 
       <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
     </div>
   );
 }
